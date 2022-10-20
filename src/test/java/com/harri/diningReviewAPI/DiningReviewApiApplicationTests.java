29cd3560b155e87972d182504faea5d95b6f11fc
Harri L <harri.liimann@gmail.com>
2022-10-20 18:46:27 +0300

    initial commit

---
+++ src/test/java/com/harri/diningReviewAPI/DiningReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harri.diningReviewAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
