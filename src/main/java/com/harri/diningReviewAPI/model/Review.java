3c68ecc3c4dd8496af650d4157e77aabb1a468ff
Harri L <harri.liimann@gmail.com>
2022-10-24 15:39:43 +0300

    define dining review entity

---
+++ src/main/java/com/harri/diningReviewAPI/model/Review.java
@@ -0,0 +1,27 @@
+package com.harri.diningReviewAPI.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String submittedBy;
+    private Long restaurantId;
+    private String review;
+
+    private Integer peanutScore;
+    private Integer eggScore;
+    private Integer dairyScore;
+}
